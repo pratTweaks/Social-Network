53afe8cf5ccf7177f029554fa537b4460d4b9659
Prateek Negi <prateekn.777@gmail.com>
2017-11-29 19:32:59 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+
+<html>
+<head>
+<title>fb</title>
+</head>
+<body>
+<form method="post" action="check.php">
+username : <input type="text" name="user"> <br>
+Password : <input type="password" name="pass"> <br>
+<input type='submit' name="submit" value="submit"/>
+</form><br>
+<a href="register.php">Need account?</a>
+</body>
+
+</html>
